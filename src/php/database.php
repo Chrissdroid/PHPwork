da6a41bc0691457c045b1c3563d9f466ab5afb56
Astroalex <alexis31.balthazard@gmail.com>
2020-12-10 17:44:44 +0100

    Adding core code

---
+++ src/php/database.php
@@ -0,0 +1,185 @@
+<?php
+$data = [
+	0 => [
+		'firstName' => 'Robert',
+		'lastName' => 'Paul',
+		'age' => 24,
+		'avatar' => [
+			'type' => 'SVG',
+			'path' => '<path fill-rule="evenodd" clip-rule="evenodd" d="M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7ZM14 7C14 8.10457 13.1046 9 12 9C10.8954 9 10 8.10457 10 7C10 5.89543 10.8954 5 12 5C13.1046 5 14 5.89543 14 7Z" fill="currentColor" /><path d="M16 15C16 14.4477 15.5523 14 15 14H9C8.44772 14 8 14.4477 8 15V21H6V15C6 13.3431 7.34315 12 9 12H15C16.6569 12 18 13.3431 18 15V21H16V15Z" fill="currentColor" />'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'light-blue',
+			'acc' => 'accent-1'
+		]
+	],
+	1 => [
+		'firstName' => 'Jeanne',
+		'lastName' => 'D\'arc',
+		'age' => 900,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'jeanne.jpg'
+		],
+		'description' => 'blablabla',
+		'theme' => [
+			'color' => 'deep-orange',
+			'acc' => 'lighten-1'
+		]
+	],
+	100 => [
+		'firstName' => 'Alexis',
+		'lastName' => 'Dr. Ternaire',
+		'age' => 18,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'alexis.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	101 => [
+		'firstName' => 'Florian',
+		'lastName' => 'Heimdall',
+		'age' => 23,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'florian.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	102 => [
+		'firstName' => 'Alexandre',
+		'lastName' => 'Slash',
+		'age' => 29,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'alexandre.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	103 => [
+		'firstName' => 'Élisa',
+		'lastName' => 'Freezy Frosty',
+		'age' => 26,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'elisa.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	104 => [
+		'firstName' => 'Jérôme',
+		'lastName' => 'Root',
+		'age' => 30,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'jerome.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	105 => [
+		'firstName' => 'Rémi',
+		'lastName' => 'Hackeye',
+		'age' => 18,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'remi.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	106 => [
+		'firstName' => 'Corinne',
+		'lastName' => 'Catcoco',
+		'age' => 32,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'corinne.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	107 => [
+		'firstName' => 'Patricia',
+		'lastName' => 'StormBits',
+		'age' => 40,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'patricia.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	108 => [
+		'firstName' => 'Hugo',
+		'lastName' => 'Byteman',
+		'age' => 30,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'hugo.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	109 => [
+		'firstName' => 'Khang',
+		'lastName' => 'Deadscreen',
+		'age' => 28,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'khang.png'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	],
+	110 => [
+		'firstName' => 'Romain',
+		'lastName' => 'Tor',
+		'age' => 29,
+		'avatar' => [
+			'type' => 'IMG',
+			'path' => 'romain.jpg'
+		],
+		'description' => "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores sequi aperiam unde aliquid placeat optio ab totam similique iusto aliquam, blanditiis atque impedit odio id voluptatibus dolores modi facilis nihil?Unde, sapiente? Voluptas quasi necessitatibus, sed id ex, adipisci quia ratione facere eveniet qui eum natus? Cum est, doloremque consectetur vero excepturi deserunt ad saepe dolores qui? Molestiae, sequi veniam?",
+		'theme' => [
+			'color' => 'amber',
+			'acc' => 'darken-2'
+		]
+	]
+];
